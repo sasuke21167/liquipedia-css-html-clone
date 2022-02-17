9b197a3edff6883b5477bab7d9545e4665872737
sasuke21167 <49262776+sasuke21167@users.noreply.github.com>
2022-02-17 19:24:50 +0700

    add project

---
+++ app/js/script.js
@@ -0,0 +1,60 @@
+
+const dropToggle=document.querySelectorAll('#drop-toggle');
+const brandMenu=document.querySelector('.brand-menu');
+const navItem=document.querySelector('.brand-desktop');
+const trending=document.querySelector('.nav-drop-trending');
+const tournament=document.querySelector('.nav-drop-tournament');
+const contribute=document.querySelector('.nav-drop-contribute');
+const shareDrop=document.querySelector('.share-drop');
+const menuDrop=document.querySelector('.menu-drop');
+
+
+const arrayShow = [brandMenu,navItem,trending,tournament,contribute,shareDrop,menuDrop];
+
+const show = (a) => {
+  arrayShow[a].classList.add("show");
+  Object.keys(arrayShow).forEach(function(item){
+    if(item!=a){
+      arrayShow[item].classList.remove("show");
+    }
+  })
+}
+
+const showTwo = (i) => {
+  let a = 1 + +i;
+  arrayShow[i].classList.add("show");
+  arrayShow[a].classList.add("show");
+  Object.keys(arrayShow).forEach(function(item){
+    if(item!=a & item!=i){
+      arrayShow[item].classList.remove("show");
+    }
+  })
+}
+
+const toggleElement = (i) => {
+  let a = 1 + +i;
+  if( i == 0){
+    if(arrayShow[i].classList.contains("show")){
+      arrayShow[i].classList.remove("show");
+      arrayShow[a].classList.remove("show");
+    }else{
+      showTwo(i);
+    }
+   
+  }else{
+    if(arrayShow[a].classList.contains("show")){
+      arrayShow[a].classList.remove("show");
+    }else{
+      show(a);
+    }
+  } 
+}
+//add event
+
+Object.keys(dropToggle).forEach(function (i) {
+  dropToggle[i].addEventListener('click', function() {
+    toggleElement(i);
+  });
+});
+
+
---
+++ notes.md
@@ -0,0 +1,7 @@
+npm install @babel/core @babel/preset-env postcss sass autoprefixer browser-sync cssnano dart-sass gulp gulp-babel gulp-postcss gulp-sass gulp-terser
+
+toggle class add display:block or display:none:
+https://stackoverflow.com/questions/57807067/use-of-pseudo-element-before-in-javascript
+
+flex box sass grid:
+https://www.toptal.com/sass/css3-flexbox-sass-grid-tutorial
