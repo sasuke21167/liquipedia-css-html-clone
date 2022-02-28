d9d9ae092be5d0814d5066414560f0b0f343bc3c
sasuke21167 <49262776+sasuke21167@users.noreply.github.com>
2022-02-28 16:18:19 +0700

    click event outside

--- app/js/script.js
+++ app/js/script.js
@@ -1,74 +1,74 @@
+const dropToggle = document.querySelectorAll("#drop-toggle");
+const brandMenu = document.querySelector(".brand-menu");
+const navItem = document.querySelector(".brand-desktop");
+const trending = document.querySelector(".nav-drop-trending");
+const tournament = document.querySelector(".nav-drop-tournament");
+const contribute = document.querySelector(".nav-drop-contribute");
+const shareDrop = document.querySelector(".share-drop");
+const menuDrop = document.querySelector(".menu-drop");
 
-const dropToggle=document.querySelectorAll('#drop-toggle');
-const brandMenu=document.querySelector('.brand-menu');
-const navItem=document.querySelector('.brand-desktop');
-const trending=document.querySelector('.nav-drop-trending');
-const tournament=document.querySelector('.nav-drop-tournament');
-const contribute=document.querySelector('.nav-drop-contribute');
-const shareDrop=document.querySelector('.share-drop');
-const menuDrop=document.querySelector('.menu-drop');
-
-
-const arrayShow = [brandMenu,navItem,trending,tournament,contribute,shareDrop,menuDrop];
+const arrayShow = [
+  brandMenu,
+  navItem,
+  trending,
+  tournament,
+  contribute,
+  shareDrop,
+  menuDrop,
+];
 
 const show = (a) => {
   arrayShow[a].classList.add("show");
-  Object.keys(arrayShow).forEach(function(item){
-    if(item!=a){
+  Object.keys(arrayShow).forEach(function (item) {
+    if (item != a) {
       arrayShow[item].classList.remove("show");
     }
-  })
-}
+  });
+};
 
 const showTwo = (i) => {
   let a = 1 + +i;
   arrayShow[i].classList.add("show");
   arrayShow[a].classList.add("show");
-  Object.keys(arrayShow).forEach(function(item){
-    if(item!=a & item!=i){
+  Object.keys(arrayShow).forEach(function (item) {
+    if ((item != a) & (item != i)) {
       arrayShow[item].classList.remove("show");
     }
-  })
-}
+  });
+};
 
 const toggleElement = (i) => {
   let a = 1 + +i;
-  if( i == 0){
-    if(arrayShow[i].classList.contains("show")){
+  if (i == 0) {
+    if (arrayShow[i].classList.contains("show")) {
       arrayShow[i].classList.remove("show");
       arrayShow[a].classList.remove("show");
-    }else{
+    } else {
       showTwo(i);
     }
-   
-  }else{
-    if(arrayShow[a].classList.contains("show")){
+  } else {
+    if (arrayShow[a].classList.contains("show")) {
       arrayShow[a].classList.remove("show");
-    }else{
+    } else {
       show(a);
     }
-  } 
-}
+  }
+};
 
-function eventClick  {
-  e.preventDefault();
+//add event
+document.onclick = myFunction;
+
+function myFunction(e) {
   console.log(e.target);
-
-  if((div.style.display === '' || div.style.display === 'none') && (e.target === button || e.target === itag)) {
-    div.style.display = 'block';    
-  } else if((div.style.display === '' || div.style.display === 'none') && (e.target !== button || e.target !== itag)) {
-    div.style.display = 'none';
-  } else {
-    div.style.display = 'none';
+  if (e.target.id !== "drop-toggle" && e.target.id !== "element") {
+    arrayShow.forEach((element) => {
+      element.classList.remove("show");
+    });
   }
 }
 
-//add event
-
 Object.keys(dropToggle).forEach(function (i) {
-  dropToggle[i].addEventListener('click', function() {
+  dropToggle[i].addEventListener("click", function () {
     toggleElement(i);
   });
 });
-
-
